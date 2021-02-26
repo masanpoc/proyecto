3a82f011559abc90b6c834cf4eb42eba8a296bcf
Mario Sancho-Cirer <marioscp1998@outlook.es>
2021-02-26 14:01:30 +0100

    fixed slider

--- src/components/Inicio/subcomponents/Carousel.js
+++ src/components/Inicio/subcomponents/Carousel.js
@@ -8,25 +8,25 @@ import { TweenLite, Power3 } from "gsap";
 
 const list = [ 
     {
-        title: 'sarabande',
-        author: 'haendel',
+        title: '"La imaginación crea la realidad."',
+        author: 'Richard Wagner',
         genre: 'barroque',
         src: Pic1
     },
     {
-        title: 'title2',
-        author: 'author2',
+        title: '"Si no fuera por la música, habría más razones para volverse loco."',
+        author: 'Tchaikovski',
         genre: 'barroque',
         src: Pic2
     },
     {
-        title: 'title3',
+        title: '"El músico es tal vez el más modesto de los animales, pero también es el más orgulloso."',
         author: 'author3',
         genre: 'barroque',
         src: Pic3
     },
     {
-        title: 'title4',
+        title: '"La música es el acto social de comunicación entre personas. Es un gesto de amistad. El más fuerte que existe."',
         author: 'author4',
         genre: 'barroque',
         src: Pic4
@@ -41,6 +41,7 @@ export const Carousel = () => {
     // useRef definitions
     let imageList = useRef();
     let contentList = useRef();
+    const mouse = useRef(null);
 
     // useState definitions
     const [state, setState] = useState({
@@ -55,7 +56,11 @@ export const Carousel = () => {
         TweenLite.to(contentList.children[0], 0, {
           opacity: 1
         });
-        nextSlide()
+        const interval = setInterval(() => {
+            mouse.current.click();
+        }, 10000);
+        return () => clearInterval(interval);
+        // nextSlide()
         // const interval = setInterval(
         //     // console.log('This will run every second!');
         //     nextSlide
@@ -64,19 +69,20 @@ export const Carousel = () => {
     }, []);
 
 
+
     // functions
     //Image transition
     const slideLeft = (index, duration, multiplied = 1) => {
         TweenLite.to(imageList.children[index], duration, {
         x: -imageWidth * multiplied,
-        ease: Power3.easeOut
+        ease: 'ease-out'
         });
     };
 
     const slideRight = (index, duration, multiplied = 1) => {
         TweenLite.to(imageList.children[index], duration, {
         x: imageWidth * multiplied,
-        ease: Power3.easeOut
+        ease: 'ease-out'
         });
     };
 
@@ -98,7 +104,7 @@ export const Carousel = () => {
     const fadeIn = (index, duration) => {
         TweenLite.to(contentList.children[index], duration, {
         opacity: 1,
-        delay: 1
+        delay: 3
         });
     };
 
@@ -106,10 +112,10 @@ export const Carousel = () => {
         if (imageList.children[0].classList.contains("active")) {
           setState({ isActive1: false, isActive2: true });
           //Image transition
-          slideLeft(0, 1);
-          slideLeft(1, 1);
-          scale(1, 1);
-          slideLeft(2, 1);
+          slideLeft(0, 3);
+          slideLeft(1, 3);
+        //   scale(1, 3);
+          slideLeft(2, 3);
           slideLeft(2, 0);
           fadeOut(0, 1);
           fadeIn(1, 1);
@@ -117,19 +123,19 @@ export const Carousel = () => {
           setState({ isActive2: false, isActive3: true });
           //Image transition
           slideRight(0, 1);
-          slideLeft(1, 1, 2);
-          slideLeft(2, 1, 2);
-          scale(2, 1);
+          slideLeft(1, 3, 2);
+          slideLeft(2, 3, 2);
+        //   scale(2, 3);
           //content transition
           fadeOut(1, 1);
           fadeIn(2, 1);
         } else if (imageList.children[2].classList.contains("active")) {
           setState({ isActive1: true, isActive3: false });
           //Image transition
-          slideLeft(2, 1, 3);
-          slideLeft(0, 1, 0);
+          slideLeft(2, 3, 3);
+          slideLeft(0, 3, 0);
           slideLeft(1, 0, 0);
-          scale(0, 1);
+        //   scale(0, 3);
           //content transition
           fadeOut(2, 1);
           fadeIn(0, 1);
@@ -137,59 +143,63 @@ export const Carousel = () => {
     };
 
     return (
-        <div className='carousel'>
-        <button onClick={nextSlide} >here</button>
-           <div className='slider'>
-               <ul ref={el => (imageList = el)} className='image-list'>
-                    <li className={state.isActive1 && "active"}>
-                        <img src={list[0].src} alt={list[0].title}></img>
-                    </li>
-                    <li className={state.isActive2 && "active"}>
-                        <img src={list[1].src} alt={list[1].title}></img>
-                    </li>
-                    <li className={state.isActive3 && "active"}>
-                        <img src={list[2].src} alt={list[2].title}></img>
-                    </li>
-                    {/* <li className={state.isActive4 && "active"}>
-                        <img src={list[3].src} alt={list[3].title}></img>
-                    </li> */}
-               </ul>
-           </div>
-           <div className='quotes'>
-                <ul ref={el => (contentList = el)} className='content-list'>
-                    <li className={state.isActive1 && "active"}>
-                        <div className='content-inner'>
-                            <h3>{list[0].title}</h3>
-                            <h4>{list[0].author}</h4>
-                            <h5>{list[0].genre}</h5>
-                        </div>
-                    </li>
-                    <li className={state.isActive2 && "active"}>
-                        <div className='content-inner'>
-                            <h3>{list[1].title}</h3>
-                            <h4>{list[1].author}</h4>
-                            <h5>{list[1].genre}</h5>
-                        </div>
-                        
-                    </li>
-                    <li className={state.isActive3 && "active"}>
-                        <div className='content-inner'>
-                            <h3>{list[2].title}</h3>
-                            <h4>{list[2].author}</h4>
-                            <h5>{list[2].genre}</h5>
-                        </div>
-                        
-                    </li>
-                    {/* <li className={state.isActive4 && "active"}>
-                        <div className='content-inner'>
-                            <h3>{list[3].title}</h3>
-                            <h4>{list[3].author}</h4>
-                            <h5>{list[3].genre}</h5>
-                        </div>   
-                    </li> */}
-                </ul>
-           </div>
-           
+        <div className='background-carousel'>
+            <div className='carousel'>
+                <button ref={mouse} onClick={nextSlide} className='carousel-bttn'>here</button>
+                
+                <div className='slider'>
+                    <ul ref={el => (imageList = el)} className='image-list'>
+                            <li className={state.isActive1 && "active"}>
+                                <img src={list[0].src} alt={list[0].title}></img>
+                            </li>
+                            <li className={state.isActive2 && "active"}>
+                                <img src={list[1].src} alt={list[1].title}></img>
+                            </li>
+                            <li className={state.isActive3 && "active"}>
+                                <img src={list[2].src} alt={list[2].title}></img>
+                            </li>
+                            {/* <li className={state.isActive4 && "active"}>
+                                <img src={list[3].src} alt={list[3].title}></img>
+                            </li> */}
+                    </ul>
+                </div>
+                
+                <div className='quotes'>
+                        <ul ref={el => (contentList = el)} className='content-list'>
+                            <li className={state.isActive1 && "active"}>
+                                <div className='content-inner'>
+                                    <h3>{list[0].title}</h3>
+                                    <h4>{list[0].author}</h4>
+                                    {/* <h5>{list[0].genre}</h5> */}
+                                </div>
+                            </li>
+                            <li className={state.isActive2 && "active"}>
+                                <div className='content-inner'>
+                                    <h3>{list[1].title}</h3>
+                                    <h4>{list[1].author}</h4>
+                                    {/* <h5>{list[1].genre}</h5> */}
+                                </div>
+                                
+                            </li>
+                            <li className={state.isActive3 && "active"}>
+                                <div className='content-inner'>
+                                    <h3>{list[2].title}</h3>
+                                    <h4>{list[2].author}</h4>
+                                    {/* <h5>{list[2].genre}</h5> */}
+                                </div>
+                                
+                            </li>
+                            {/* <li className={state.isActive4 && "active"}>
+                                <div className='content-inner'>
+                                    <h3>{list[3].title}</h3>
+                                    <h4>{list[3].author}</h4>
+                                    <h5>{list[3].genre}</h5>
+                                </div>   
+                            </li> */}
+                        </ul>
+                </div>
+                
+            </div>
         </div>
     )
 }
