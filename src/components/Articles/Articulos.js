92e04f04eadc4d6dc1ba455d9bb9d2de5c284d3f
Mario Sancho-Cirer <marioscp1998@outlook.es>
2021-03-17 00:24:29 +0100

    articles going forward!

--- src/components/Articles/Articulos.js
+++ src/components/Articles/Articulos.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useLocation, withRouter } from 'react-router-dom';
 import './Articulos.scss';
 import { Articulo1 } from './Lista/Articulo1';
 import { Lista } from './Lista';
+
+function _ScrollToTop(props) {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return props.children
+}
+const ScrollToTop = withRouter(_ScrollToTop)
 
 export const Articulos = () => {
 
     return (
         <Router>
+            <div>
+            <ScrollToTop>
             <Switch>
                 <Route path='/articulos' exact component={Lista} />
                 <Route path='/articulos/1' component={Articulo1} />
@@ -15,6 +26,8 @@ export const Articulos = () => {
                     <Redirect to="/articulos" />
                 </Route>
             </Switch>
+            </ScrollToTop>
+            </div>
         </Router>
     )
 }
--- src/components/Articles/Lista.js
+++ src/components/Articles/Lista.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useLocation } from 'react-router-dom';
 import './Articulos.scss';
 
 export const Lista = () => {
     return (
-        <div className='lista' style={{'minHeight': '100vh'}}>
-            <div className='intro'>
-                <h1>Artículos sobre música</h1>
-                <h3>Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</h3>
-                <h2>Descúbrelos aquí abajo</h2>
+        <div className='lista'>
+            <div className='intro-articles'>
+                <h1 className='heading'>Artículos sobre música</h1>
+                <p className='description'>Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
+                <h2 className='info'>Descúbrelos aquí abajo</h2>
+                <svg style={{"width":"24px", "height":"24px"}} viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M9,4H15V12H19.84L12,19.84L4.16,12H9V4Z" />
+                </svg>
             </div>
             <div className='article-1'>
-                <h1>Titulo que sea sobre orquesta por ejemplo</h1>
-                <p className='abstract-0'>Resumen</p>
-                <p className='abstract' >Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
-                <Link className='link' to='/articulos/1'>Leer artículo</Link>
+                <div className='cropped-1'>
+                    <h3 className='title'>Titulo que sea sobre orquesta por ejemplo</h3>
+                    <p className='abstract-0'>Resumen:</p>
+                    <p className='abstract' >Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
+                    <Link className='link' to='/articulos/1'> {'>> '}Leer artículo</Link>
+                </div>
+                <div className='shadowing'></div>
             </div>
-            <br />
-            hhh
+            <div className='article-2'>
+                <div className='cropped-2'>
+                    <h3 className='title'>Titulo que sea sobre orquesta por ejemplo</h3>
+                    <p className='abstract-0'>Resumen</p>
+                    <p className='abstract' >Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
+                    <Link className='link' to='/articulos/1'>{'>> '}Leer artículo</Link>
+                </div>
+            </div>
+            <div className='article-3'>
+                <div className='cropped-3'>
+                    <h3 className='title'>Titulo que sea sobre orquesta por ejemplo</h3>
+                    <p className='abstract-0'>Resumen</p>
+                    <p className='abstract' >Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
+                    <Link className='link' to='/articulos/1'>{'>> '}Leer artículo</Link>
+                </div>
+                <div className='shadowing'></div>
+            </div>
+            <div className='article-4'>
+                <div className='cropped-4'>
+                    <h3 className='title'>Titulo que sea sobre orquesta por ejemplo</h3>
+                    <p className='abstract-0'>Resumen</p>
+                    <p className='abstract' >Algunos de nuestros miembros han escrito artículos increíbles sobre música clásica</p>
+                    <Link className='link' to='/articulos/1'>{'>> '}Leer artículo</Link>
+                </div>
+            </div>
+
+            <div className='loader'>
+                Botón de cargar más
+            </div>
            
         </div>
     )
